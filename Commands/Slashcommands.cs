6c14744a1b963848b75707e93e5f9866d725c4a5
J_M_Lutra <jmlutra@gmail.com>
2021-10-10 01:39:13 +0200

    Added /about

--- Commands/Slashcommands.cs
+++ Commands/Slashcommands.cs
@@ -17,7 +17,7 @@ namespace TemplateBot.Commands
         [SlashCommand("ping", "Send's the actual ping")]
         public static async Task Ping(InteractionContext ctx)
         {
-            await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().AsEphemeral(true).WithContent("Loading Ping, could take time. Please lay back <3"));
+            await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().AsEphemeral(true).WithContent("Loading Ping, could take time. Please wait."));
             await Task.Delay(2000);
             await ctx.Channel.SendMessageAsync($"Pong: `{Client.Ping}ms`");
         }
@@ -30,13 +30,43 @@ namespace TemplateBot.Commands
             {
                 await Task.Delay(5000);
                 await ctx.EditResponseAsync(new DiscordWebhookBuilder().WithContent("Shutdown request accepted."));
+                await Task.Delay(2000);
                 ShutdownRequest.Cancel();
-                await ctx.EditResponseAsync(new DiscordWebhookBuilder().WithContent("Shuting down!"));
+                await ctx.EditResponseAsync(new DiscordWebhookBuilder().WithContent("Shutting down!"));
             }
             else
             {
                 await ctx.EditResponseAsync(new DiscordWebhookBuilder().WithContent("You are not allowed to execute this request!"));
             }
+        }
+
+        [SlashCommand("about", "Informations about the Bot")]
+        public static async Task About(InteractionContext ctx)
+        {
+            await ctx.CreateResponseAsync(InteractionResponseType.DeferredChannelMessageWithSource);
+            var bot = ctx.Client;
+            var owner = (bot.CurrentApplication.Owners.Count() == 1) ? bot.CurrentApplication.Team.Owner.Username : bot.CurrentApplication.TeamName;
+            var embed = new DiscordEmbedBuilder();
+            embed.WithAuthor(bot.CurrentUser.UsernameWithDiscriminator, null, ctx.Client.CurrentUser.AvatarUrl)
+                .WithTitle($"Informations about {bot.CurrentUser.Username}")
+                .WithTitle($"The Owner of the Bot is {owner}")
+                .AddField("Number of Guilds:", $"`{bot.Guilds.Count}`", true)
+                .AddField("Number of Commands:", $"`{bot.GetApplicationCommands().RegisteredCommands.First().Value.Count}`", true)
+                .AddField("The Dev(s):", $"{getDevs(bot.CurrentApplication)}")
+                .AddField("Library:", $"The Bot was written in C# using the {Formatter.MaskedUrl(bot.BotLibrary, new Uri("https://github.com/Aiko-IT-Systems/DisCatSharp"))} Library. \n The Template fot this Bot can be found {Formatter.MaskedUrl("here", new Uri("https://github.com/Aiko-It-Systems/DisCatSharp.TemplateBot"))}.")
+                .WithTimestamp(DateTime.Now)
+                .WithColor(DiscordColor.Azure);
+            await ctx.EditResponseAsync(new DiscordWebhookBuilder().AddEmbed(embed));
+        }
+
+        private static String getDevs(DiscordApplication bot)
+        {
+            String devs = null;
+            foreach (var dev in bot.Team.Members)
+            {
+                devs = $"{devs}\n{dev.User.Mention}";
+            };
+            return devs;
         }
     }
 }
